76cf509a5580055545cfd8e5726117c696591d51
Joan Palomares <joanqpalomares@gmail.com>
2025-02-11 19:22:09 +0000

    implement CQL service to insert employee data

--- srv/cqlService.cds
+++ srv/cqlService.cds
@@ -3,7 +3,9 @@ using {procurement.db.master} from '../db/datamodel';
 service NewCQLService {
 
     @readonly
-    entity readEmployee as projection on master.employee;
+    entity readEmployee   as projection on master.employee;
 
+    @insertonly
+    entity insertEmployee as projection on master.employee;
 
 }
--- srv/cqlService.js
+++ srv/cqlService.js
@@ -1,14 +1,48 @@
+const { error } = require('@sap/cds');
+const { insert } = require('@sap/cds');
 const cds = require('@sap/cds');
-const {employee} = cds.entities("procurement.db.master");
+const { message } = require('@sap/cds/lib/log/cds-error');
+const { rewrite } = require('@sap/cds/libx/_runtime/db/generic');
+const { employee } = cds.entities("procurement.db.master");
 
-const NewCQLService= function(srv){
+const NewCQLService = function (srv) {
     // read data from employee table
-    srv.on("READ", "readEmployee", async(req, res)=>{
+    srv.on("READ", "readEmployee", async (req, res) => {
         var results = [];
-        results = await cds.tx(req).run(SELECT.from(employee))
+
+        // option 1
+        results = await SELECT.from(employee)
+
+        // option 2
+        // let query = SELECT.from (employee);
+        // let employees = await cds.db.run(query)
+        // return employees
+
+
+        // with where
+        // results = await SELECT.from (employee) .where({"salaryAmount": "55,000"})
 
         return results;
     })
+
+    // insert employee data
+    srv.on("CREATE", "insertEmployee", async (req) => {
+        try {
+            const result = await cds.transaction(req).run(
+                INSERT.into(employee).entries(req.data)  
+            );
+
+            // Check if the insertion was successful
+            if (result) {
+                return req.data; 
+            } else {
+                throw req.error(500, "There was an error inserting the record.");
+            }
+        } catch (err) {
+            throw req.error(500, "An error occurred: " + err.message);
+        }
+    });
 }
 
-module.exports=NewCQLService;
+
+module.exports = NewCQLService;
